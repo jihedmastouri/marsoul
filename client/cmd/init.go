9f8120fddd6e4e5d6013eb3970c0729cfa1861aa
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2023-11-10 10:00:46 +0100

    yestardays prog

--- client/cmd/init.go
+++ client/cmd/init.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/jihedmastouri/marsoul/client/internal"
+	"github.com/jihedmastouri/marsoul/client/internal/helpers"
 	"github.com/spf13/cobra"
 )
 
@@ -30,36 +31,53 @@ var initCmd = &cobra.Command{
 		}
 
 		if filePath != "" {
-			providedFile, err := os.Open(filePath)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Opening file failed with: ", err)
+			if err := FileArgs(filePath, args); err != nil {
+				fmt.Fprintln(os.Stderr, "Reading file failed with: ", err)
 				os.Exit(1)
 			}
-			defer providedFile.Close()
-			scanner := bufio.NewScanner(providedFile)
-			for scanner.Scan() {
-				line := scanner.Text()
-				args = append(args, line)
-			}
-			if err := scanner.Err(); err != nil {
-				fmt.Fprintln(os.Stderr, "Reading file content failed with: ", err)
-				os.Exit(1)
-			}
-
 		}
 
-		if valid, addrs := internal.ValidateAdrs(args...); !valid {
+		if valid, addrs := internal.ValidateAdrs(args); !valid {
 			fmt.Fprintln(os.Stderr, "Argument passed is not a valid address: ", addrs)
 			os.Exit(1)
 		}
 
 		file, err := internal.CreateConfigFile(internal.ResolversFile)
+		if err != nil {
+			if !os.IsExist(err) {
+				fmt.Fprintln(os.Stderr, "creating config file `$HOME/.marsoul/resolvers` failed with :", err)
+				os.Exit(1)
+			}
+			s, err := helpers.ReadStdin("file exists already. Would you like to (A)dd to it or (R)eplace: (A/R)")
+			if err != nil {
+				helpers.ErrExit("reading option failed", err)
+			}
+		}
 
 		writable := strings.Join(args, "\n")
 		_, err = file.WriteString(writable)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Writing to file `$HOME/.marsoul/resolvers` failed with: ", err)
-			os.Exit(1)
+			helpers.ErrExit("writing to file `$HOME/.marsoul/resolvers` failed with: ", err)
 		}
 	},
+}
+
+func FileArgs(filePath string, args []string) error {
+	providedFile, err := os.Open(filePath)
+	if err != nil {
+		return err
+
+	}
+	defer providedFile.Close()
+
+	scanner := bufio.NewScanner(providedFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		args = append(args, line)
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
